04db17523667a52b198b18d75af1e8869f2e65fd
Pauline Shen <s9633291@gmail.com>
2022-08-19 03:10:13 +0000

    implemented map as a function

---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+
+const results2 = map([], word => word[0]);
+assertArraysEqual(results2, []);
+
+const results3 = map([1, 2, 3, 4, 5], n => n * n);
+assertArraysEqual(results3, [1, 4, 9, 16, 25]);
