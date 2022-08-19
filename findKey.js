4f52b8ac220a38ca83598f1e16caf419bb33e805
Pauline Shen <s9633291@gmail.com>
2022-08-19 03:39:05 +0000

    return the first key that callback returns true

---
+++ findKey.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const findKey = function(obj, callback) {
+  let find = undefined;
+  for (var o in obj) {
+    if (callback(obj[o])) {
+      find = o;
+      return find;
+    }
+  } 
+  return find;
+};
+
+
+let res1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+assertEqual(res1, "noma");
+
+
+let res2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4);
+assertEqual(res2, undefined);
