cd3b7377c09e0b6bf3163a0922842ecfefa1274b
Pauline Shen <s9633291@gmail.com>
2022-08-27 21:36:39 +0000

    refactored code

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   if (arr.length === 0) {
@@ -12,6 +6,3 @@ const head = function(arr) {
   }
   return arr[0];
 };
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
