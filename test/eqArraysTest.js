703bad2d7c9303b642caedfeab564fff9dc9cdf0
Pauline Shen <s9633291@gmail.com>
2022-08-30 03:31:02 +0000

    changed tests to mocha

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,18 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
+
+describe("#eqArrays", () => {
+  it("returns true for ([1, 2, 3], [1, 2, 3])", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns false for ([1, 2, 3], [3, 2, 1])", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+  it("returns true for (['1', '2', '3'], ['1', '2', '3'])", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+  it("returns false for (['1', '2', '3'], ['1', '2', 3])", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,9 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+});
