74aac4d8795bea273d22256dffa690e60e27c082
Pauline Shen <s9633291@gmail.com>
2022-08-30 02:46:32 +0000

    refactored code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -14,3 +7,5 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
+
+module.exports = assertArraysEqual;
